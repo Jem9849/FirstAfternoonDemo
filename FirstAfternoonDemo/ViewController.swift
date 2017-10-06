364c9f83fdbf3173c02bab6e0984beca5b879e78
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-06 13:34:19 -0600

    Color changing stuff and things like background and button and stuff I guess idk probably lolz

--- FirstAfternoonDemo/ViewController.swift
+++ FirstAfternoonDemo/ViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    var count = 0
     @IBOutlet weak var firstButton: UIButton!
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        
+        if (count % 2 == 0)
+        {
+        firstButton.backgroundColor = .green
+        count += 1
+        }
+        else if (count % 2 == 1)
+        {
+        firstButton.backgroundColor = .blue
+        count += 1
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
