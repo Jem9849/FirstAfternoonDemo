f61637aee01a0e170f3b6c574f172c0b34ae2991
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-06 12:41:45 -0600

    First commit but I actually added color and a button and a picture beforehand.

---
+++ FirstAfternoonDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FirstAfternoonDemo
+//
+//  Created by Saysanasy, Jeremy on 10/6/17.
+//  Copyright © 2017 Saysanasy, Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
